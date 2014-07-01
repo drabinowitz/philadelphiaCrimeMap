1fd956f5e1a37601252f5125240f1300aef6c451
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-06-30 21:30:54 -0400

    defined boundary event

--- app.js
+++ app.js
@@ -10,15 +10,17 @@ $(document).ready(function(){
 
 		  minZoom: 13,
 
-		  draggable: false,
+		  draggable: true,
 
 		  overviewMapControl: false,
 
-		  panControl: false
+		  panControl: true
 
 		};
 
 		var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+
+		var allowedBounds;
 
 		var marker = new google.maps.Marker({
 
@@ -30,6 +32,10 @@ $(document).ready(function(){
 
 		});
 
+		google.maps.event.addListenerOnce(map, 'idle', function() {
+         allowedBounds = map.getBounds();
+      });
+
 		google.maps.event.addListener(marker,'click',function(){
 
 			map.setZoom(8);
@@ -40,35 +46,46 @@ $(document).ready(function(){
 
 		google.maps.event.addListener(map,'click',function(event){
 
-		  var newMarker = new google.maps.Marker({
+			var newMarker = new google.maps.Marker({
 
-		  position: event.latLng,
+				position: event.latLng,
 
-		  map: map,
+				map: map,
 
-		  title: 'Click to zoom'
+				title: 'Click to zoom'
+
+			});
 
 		});
 
-		});
+		google.maps.event.addListener(map,'center_changed',function() { checkBounds(); });
+
+		function checkBounds() {   
+
+    		if(! allowedBounds.contains(map.getCenter())) {
+		      var C = map.getCenter();
+		      var X = C.lng();
+		      var Y = C.lat(); 
+
+		      var AmaxX = allowedBounds.getNorthEast().lng();
+		      var AmaxY = allowedBounds.getNorthEast().lat();
+		      var AminX = allowedBounds.getSouthWest().lng();
+		      var AminY = allowedBounds.getSouthWest().lat();
+
+		      // console.log( 'X,Y value before change ' + X + ',' + Y  );
+   		      // console.log( 'AmaxX,AmaxY and AminX,AminY value before change ' +  AminX + ',' + AminY + ' and ' + AmaxX + ',' + AmaxY  );
+
+		      if (X < AminX) {X = AminX;}
+		      if (X > AmaxX) {X = AmaxX;}
+		      if (Y < AminY) {Y = AminY;}
+		      if (Y > AmaxY) {Y = AmaxY;}
+
+		      // console.log( 'X,Y value after change ' + X + ',' + Y  );
+
+		      map.setCenter(new google.maps.LatLng(Y,X));
+    		}
+		}
 
 	})();
-
-	var result = $.ajax({
-		url: "https://maps.googleapis.com/maps/api/place/textsearch/json",
-		data: {
-
-			query:"Philadelphia",
-
-			key:"AIzaSyCeONSkk-jDozekhXZffdnTgbqOyVe7KH0"
-
-		},
-		dataType: "jsonp",
-		type: "GET"
-		}).done(function(result){
-			debugger;
-			alert(result.results.length);
-
-		});
 
 });

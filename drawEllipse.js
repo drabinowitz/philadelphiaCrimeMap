314517653776cbffcd6affe31c6766d5fb248f0a
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-07-03 09:16:08 -0400

    refactored draw tools

--- drawEllipse.js
+++ drawEllipse.js
@@ -1,41 +1,70 @@
+/*function fluidZoom( zoomLevel,stepSize,stepSpeed ){
+
+	var zoomStep = map.getZoom();
+
+	if( zoomStep >= zoomLevel - stepSize ){
+
+		map.setZoom(zoomLevel);
+
+	} else {
+
+		zoomStep += stepSize - ( zoomLevel - zoomStep ) % stepSize;
+
+		map.setZoom(zoomStep);
+
+		setTimeout( function(){
+
+			fluidZoom( zoomLevel,stepSize,stepSpeed );
+
+		}, stepSpeed );
+
+	}
+
+}*/
+
+function drawPerimeter( lat,lng,accuracy,scale ){
+
+	if (accuracy >= 3 && Math.floor(accuracy) == accuracy){
+
+		var coordinates = []
+
+		for(var i = 0; i <= accuracy;i++){
+
+			coordinates.push([
+
+				lat + scale * Math.cos( 2 * Math.PI * i / accuracy ),
+
+				lng + scale * Math.sin( 2 * Math.PI * i / accuracy )
+
+			]);
+
+		}
+
+		return coordinates;
+
+	} else {
+
+		console.log('invalid accuracy value of ' + accuracy + ' accuracy must be an integer value of 3 or greater');
+
+	}
+
+}
+
 function drawEllipse( latLng1,latLng2,scale,accuracy ){
 
-/*		if (accuracy >= 3 && Math.floor(accuracy) == accuracy){
-
-			var coordinates = []
-
-			for(var i = 0; i <= accuracy;i++){
-
-				coordinates.push([
-
-					lat + scale * Math.cos( 2 * Math.PI * i / accuracy ),
-
-					lng + scale * Math.sin( 2 * Math.PI * i / accuracy )
-
-				]);
-
-			}
-
-			return coordinates;
-
-		} else {
-
-			console.log('invalid accuracy value of ' + accuracy + ' accuracy must be an integer value of 3 or greater');
-
-		}*/
 	accuracy = 2 * accuracy;
 
-	var coordinates = []
+	var coordinates = [];
 
-	var theta = Math.atan( ( latLng2[1]-latLng1[1] ) / ( latLng2[0]-latLng1[0] ) )
+	var theta = Math.atan( ( latLng2[1]-latLng1[1] ) / ( latLng2[0]-latLng1[0] ) );
 
 	for(var i = 0; i <= accuracy;i++){
 
 		coordinates.push([
 
-			latLng1[0] + scale * Math.cos( theta - Math.PI * i / accuracy + Math.PI / 2 ),
+			latLng1[0] + scale * Math.cos( theta - Math.PI * i / accuracy - Math.PI / 2 ),
 
-			latLng1[1] + scale * Math.sin( theta - Math.PI * i / accuracy + Math.PI / 2 )
+			latLng1[1] + scale * Math.sin( theta - Math.PI * i / accuracy - Math.PI / 2 )
 
 		]);
 
@@ -45,50 +74,17 @@ function drawEllipse( latLng1,latLng2,scale,accuracy ){
 
 		coordinates.push([
 
-			latLng2[0] + scale * Math.cos( theta - Math.PI * j / accuracy - Math.PI / 2 ),
+			latLng2[0] + scale * Math.cos( theta - Math.PI * j / accuracy + Math.PI / 2 ),
 
-			latLng2[1] + scale * Math.sin( theta - Math.PI * j / accuracy - Math.PI / 2 )
+			latLng2[1] + scale * Math.sin( theta - Math.PI * j / accuracy + Math.PI / 2 )
 
 		]);
 
 	}
 
-	coordinates.push([
-
-		latLng1[0] + scale * Math.cos( theta + Math.PI / 2 ),
-
-		latLng1[1] + scale * Math.sin( theta + Math.PI / 2 )
-
-	]);
+	coordinates.push(coordinates[0]);
 
 	return coordinates;
 
 }
 
-var coordinatesEllipse = drawEllipse( [39.9578866,-75.1698302],[39.9608866,-75.1728302],0.001,8 );
-
-var perimeterCoordinatesEllipse = [];
-
-$.each(coordinatesEllipse,function(index,value){
-
-	perimeterCoordinatesEllipse.push( new google.maps.LatLng(value[0],value[1]) )
-
-})
-
-perimeter = new google.maps.Polygon({
-
-	paths: perimeterCoordinatesEllipse,
-
-	strokeColor: '#FF0000',
-
-	strokeOpacity: 0.8,
-
-	strokeWeight: 2,
-
-	fillColor: '#FF0000',
-
-	fillOpacity: 0.35
-
-});
-
-perimeter.setMap(gMap);
